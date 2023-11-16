3e516d4bdf0a859cf5042e4a87af1082c296850c
ghosharatrika <150947458+ghosharatrika@users.noreply.github.com>
2023-11-16 14:02:58 +0530

    Create exception_handling.py

---
+++ exception_handling.py
@@ -0,0 +1,7 @@
+# To divide 1 by 0,1,2,3,4,5
+
+for i in range(0, 6):
+    try:
+        print("1 /", i, ":", 1/i)
+    except ZeroDivisionError:     # To prevent the code from crashing due to zero division error
+        print("Indeterminate.")
