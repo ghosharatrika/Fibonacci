b16647b80189e3efff12fed5e0141d551a55bc8d
ghosharatrika <150947458+ghosharatrika@users.noreply.github.com>
2023-11-16 14:07:56 +0530

    Create fibonacci.py

---
+++ fibonacci.py
@@ -0,0 +1,7 @@
+fibonacci_numbers = [0, 1]  # list of fibonacci no. with two initial values created
+
+while len(fibonacci_numbers) < 10:
+      next_number = fibonacci_numbers[-1] + fibonacci_numbers[-2]  # Next fibonacci no is created
+      fibonacci_numbers.append(next_number)                        # Next term is added to the list
+
+print(fibonacci_numbers) # the entire list of 10 fibonacci no. is printed on the screen
